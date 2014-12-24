e56a488db2fb4cb1dc98de0d2ee8108c36b27b7b
Alex Kachura <alex.kachura@gmail.com>
2014-12-24 15:52:46 +0200

    facebook autogrow

--- public/js/lib/facebook.js
+++ public/js/lib/facebook.js
@@ -746,7 +746,7 @@
         var view = document.getElementById('view');
         view && $facebook.grow(Math.max(view.offsetHeight, view.clientHeight), self.isTab || self.isCanvas);
     }, 500);};
-    $rootScope.$on('$routeChangeSuccess', self.isTab || self.isCanvas && onGrow);
+    $rootScope.$on('$routeChangeSuccess', (self.isTab || self.isCanvas) && onGrow);
 }]);
 
 !function(options){
